a38a13a5045b826f80bdd6fc645caca5fffe8219
ofir zvi shaboo <ofir@Yarons-MacBook-Air.local>
2023-05-29 20:52:28 +0300

    firstcommit

---
+++ project.py
@@ -0,0 +1,80 @@
+#from functions import get_todos, write_todos
+import functions
+import time
+
+now = time.strftime("%d  %b - %Y %H:%M:%S ")
+print("It is", now)
+while True:
+    user_action = input("Type add, show, complete, edit or exit: ")
+    user_action = user_action.strip()
+
+    if user_action.startswith('add'):
+            todo = user_action[4:]
+
+            todos = functions.get_todos()
+            
+            todos.append(todo + '\n')
+            
+            functions.write_todos(todos)
+
+    elif user_action.startswith('show'):
+            
+            todos = functions.get_todos()
+
+            ##new_todos = [item.strip('\n') for item in todos]
+            
+            for index, item in enumerate(todos):
+                item = item.strip('\n')
+                row = f"{index+1}-{item}"
+                print(row)
+    elif user_action.startswith('edit'):
+        try:
+            number = int(user_action[5:])
+            number = number - 1
+
+            todos = functions.get_todos()
+                    
+            new_todo = input("Enter a new todo:")
+            todos[number] = new_todo + '\n'
+                
+            functions.write_todos(todos)
+
+        except ValueError:
+            print("your command is not valid")
+            continue
+
+    elif user_action.startswith('complete'):
+        try:
+            number = int(user_action[9:])
+
+            todos = functions.get_todos()
+
+            index = number - 1
+            todo_to_remove = todos[index].strip('/n')
+            todos.pop(index)
+
+            functions.write_todos(todos)
+
+            message = f"The todo {todo_to_remove} was removed from the list"
+            print(message)
+        except IndexError:
+            print("There is no item with that number")
+            continue
+    elif user_action.startswith('exit'):
+        break
+
+    else:
+        print("F U")
+
+print("Bye!")
+
+
+##while True:
+    ##todo = input(user_prompt)
+    ##print(todo.capitalize()) caps first letter in first word
+    ##print(todo.title()) all first letters in words caps
+    ##dir(type[]) to check methoods
+    ##import builtins
+    ## 'show' | 'display'
+
+

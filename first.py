18fc7602f65a33a1e0f94428b48440624af8d2f2
Ofir <ofirfirst@gmail.com>
2023-07-05 18:38:59 +0300

    projectt

--- first.py
+++ first.py
@@ -1,16 +1,52 @@
-# name = "Ofir"
-# name1 = "jg"
-a = 22
-# #print(str(a) + name1)
-b = 23
-# print(a*b)
-# print(a+b)
-# print(a/b)
-# print(a-b)
-# print(f"my name is: {name} my age is: {a}")
-if a == 12:
-    print(22)
-elif a == 22:
-    print(21)
-else:
-    print(333)
+from functions import USD, ILS
+import time
+
+now = time.strftime("%d  %b - %Y %H:%M:%S ")
+print("It is", now)
+
+# def should_stay():
+#     user_dec = input("Do you want to do another calculation? y/n")
+#     if user_dec == "y" or user_dec == "Y":
+#         continue
+#     elif user_dec == "n" or user_dec == "N":
+#         break
+
+def get_user_value():
+    restart = True
+
+    while restart:
+        user_choice = input("1 - D to S / 2 - S to D: ")
+        if user_choice == "1":
+            value_to_convert = float(input("Enter quantity to convert: "))
+            usd = USD()
+            result = usd.calculate(value_to_convert)
+            print(result)
+            return result
+        elif user_choice == "2":
+            value_to_convert = float(input("Enter quantity to convert: "))
+            ils = ILS()
+            result = ils.calculate(value_to_convert)
+            print(result)
+            return result
+        else:
+            print("wrong input")
+            continue
+
+def main():
+    restart = True
+    mylist = []
+    while restart:
+        mylist.append(get_user_value())
+        choice = input("Would you like to start over? y/n: ")
+        if choice == "y":
+            print(mylist)
+            continue
+        elif choice == "n":
+            print(mylist)
+            return mylist
+        else:
+            choice = input("Would you like to start over? y/n: ")
+
+
+
+main()
--- functions.py
+++ functions.py
@@ -1,46 +1,15 @@
-def run1():
-    print("Hello From My Function!")
+class USD:
+    def get_value(self):
+        return 3.52
 
 
-def run2():
-    return "Hello"
+    def calculate(self, user_input):
+        return user_input * self.get_value()
+class ILS:
+    def get_value(self):
+        return 0.28
 
 
-def run3(name):
-    print(name)
+    def calculate(self, user_input):
+        return user_input * self.get_value()
 
-
-def run4(name):
-    return "Hello " + name
-
-
-run1()
-my_word = run2()
-run3('daniel')
-my_word = run4('daniel')
-
-
-# empty return
-def r():
-    if 1 > 0:
-        print(1)
-        return
-
-
-if 1 > 0:  # will never run!
-    print(2)
-
-
-# pass
-def still_developing():
-    pass
-
-
-# yield
-def simple_fun():
-    yield 1
-    yield 2
-
-
-for value in simple_fun():
-    print(value > 1)
